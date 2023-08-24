d7b5d5c15d93fc5912fe2f8b78696d794a675303
beeee000 <beeee000@163.com>
2023-08-24 05:21:08 +0000

    update test/user/favor_list.py.
    Signed-off-by: beeee000 <beeee000@163.com>

--- test/user/favor_list.py
+++ test/user/favor_list.py
@@ -22,8 +22,6 @@ def test_favor_list():
         # 如果 status_code = 0, 则表示该接口没有问题
         if data["status_code"] == 0:
             print("true")
-        # 用户令牌在某些服务中是必须的, 请在config.py下手动修改token的值
-        #print(data["token"])
         return end_time - start_time
     else:
         print("请求失败")
